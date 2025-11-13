0b9035bfe7381833436dddf294e278226e1ef683
oksatri <oksatriwahyu@gmail.com>
2025-11-13 23:56:45 +0700

    [rev] header

--- resources/views/front/_parts/header.blade.php
+++ resources/views/front/_parts/header.blade.php
@@ -52,7 +52,7 @@
         <div class="container-fluid">
             <div class="row">
 
-                <div class="col-md-2">
+                <div class="col-xs-2">
                     <div class="main-logo">
                         <a href="{{ url('/') }}">
                             @php
@@ -69,7 +69,7 @@
                     </div>
                 </div>
 
-                <div class="col-md-10">
+                <div class="col-xs-10">
 
                     <nav id="navbar">
                         <div class="main-menu stellarnav">
