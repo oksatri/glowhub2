875273beaa63ff2e3e7208dd230e4b7c9945e369
oksatri <oksatriwahyu@gmail.com>
2025-11-19 20:32:51 +0700

    [rev]

--- config/filesystems.php
+++ config/filesystems.php
@@ -40,7 +40,7 @@ return [
 
         'public' => [
             'driver' => 'local',
-            'root' => base_path('../public_html/storage/app/public'),
+            'root' => storage_path('app/public'),
             'url' => env('APP_URL').'/storage',
             'visibility' => 'public',
             'throw' => false,
