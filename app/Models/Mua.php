70be176ec06c3008deae0110757677296c81631f
oksatri <oksatriwahyu@gmail.com>
2025-11-22 21:19:02 +0700

    [rev]

--- app/Models/Mua.php
+++ app/Models/Mua.php
@@ -24,7 +24,7 @@ class Mua extends Model
 
     public function user()
     {
-        return $this->hasOne(\App\Models\User::class, 'user_id');
+        return $this->hasOne(\App\Models\User::class, 'id','user_id');
     }
 
     public function services()
