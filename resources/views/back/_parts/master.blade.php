22bda9dac291b863cb123e11b70e28a9df67d73e
Oksa Triwahyu <oksatriwahyu@gmail.com>
2026-04-24 09:25:49 +0700

    [rev]

--- resources/views/back/_parts/master.blade.php
+++ resources/views/back/_parts/master.blade.php
@@ -146,6 +146,17 @@
                     </div>
                 @endif
 
+                @if (session('error'))
+                    <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                        <i class="fas fa-exclamation-triangle me-2"></i>
+                        {!! session('error') !!}
+                        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"
+                            onclick="this.closest('.alert').style.display='none';">
+                            <i class="fas fa-times"></i>
+                        </button>
+                    </div>
+                @endif
+
 
                 <div class="container-fluid">
                     @yield('content')
